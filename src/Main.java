ce8bd847070efe54a8c6680d358cfd411df8b2cd
DavGuett <soulsec@protonmail.com>
2023-02-15 21:54:48 -0300

    Tela de menu criada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import telas.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bem vindo!");
+        System.out.println("-------------------");
+        var menu = new Menu();
+        menu.mostrarOpcoes();
     }
 }
---
+++ src/telas/Menu.java
@@ -0,0 +1,9 @@
+package telas;
+
+public class Menu {
+    public void mostrarOpcoes(){
+        System.out.println("Opção 1 - Incluir item");
+        System.out.println("Opção 2 - Excluir item");
+        System.out.println("Opção 3 - Listar itens");
+    }
+}
