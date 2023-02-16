09b7b7adb8317bb3f96758eaf1ea190c09d654d1
DavGuett <soulsec@protonmail.com>
2023-02-16 20:09:09 -0300

    Conexão com banco de dados criada, banco criado

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 import telas.*;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import metodosParaOBancoDeDados.Metodos;
 public class Main {
+
     public static void main(String[] args) {
+        var criaBanco = new Metodos();
+        criaBanco.criarBanco("testando.db");
         System.out.println("Bem vindo!");
         System.out.println("-------------------");
         var menu = new Menu();
---
+++ src/metodosParaOBancoDeDados/Metodos.java
@@ -0,0 +1,21 @@
+package metodosParaOBancoDeDados;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Metodos {
+    public static void criarBanco(String nomeDoBanco) {
+        String url = "jdbc:sqlite:db/" + nomeDoBanco;
+        try (Connection conn = DriverManager.getConnection(url)){
+            if (conn != null) {
+                DatabaseMetaData metaDados = conn.getMetaData();
+                System.out.println("O nome do driver do banco é " + metaDados.getDriverName());
+                System.out.println("Um novo banco de dados foi criado");
+            }
+        } catch (SQLException mensagem) {
+            System.out.println(mensagem.getMessage());
+        }
+    }
+}
