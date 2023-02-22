b571dbf75bdd5dd8147cdc98d9c84014e5337dd4
DavGuett <soulsec@protonmail.com>
2023-02-22 15:51:27 -0300

    Menu agora roda até o usuário decidir sair da aplicação

--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,30 @@ public class Main {
         System.out.println("Bem vindo!");
         System.out.println("-------------------");
         var menu = new Menu();
-        menu.showOptions();
-        Scanner getInput = new Scanner(System.in);
-        int option = getInput.nextInt();
-        if (option == 1) {
-            var incluirMenu = new AddItem();
-            incluirMenu.askForProductName();
-        } else if (option == 2) {
-            var excluirMenu = new DeleteItem();
-            excluirMenu.askForProductID();
+        boolean run = true;
+        while(run) {
+            menu.showOptions();
+            Scanner getInput = new Scanner(System.in);
+            int option = getInput.nextInt();
+            if (option == 1) {
+                var incluirMenu = new AddItem();
+                incluirMenu.askForProductName();
+            } else if (option == 2) {
+                var excluirMenu = new DeleteItem();
+                excluirMenu.askForProductID();
+            }
+            else if (option == 3) {
+                var databaseTableSelectAll = new Select();
+                databaseTableSelectAll.selectAll();
+            } else if (option == 4){
+                System.out.println("Saindo...");
+                run = false;
+            }
+            else {
+                System.out.println("Digite uma opção válida");
+            }
+
         }
-        else if (option == 3) {
-            var databaseTableSelectAll = new Select();
-            databaseTableSelectAll.selectAll();
-        } else {
-            System.out.println("Digite uma opção válida");
         }
 
-    }
 }
--- src/telas/Menu.java
+++ src/telas/Menu.java
@@ -5,5 +5,6 @@ public class Menu {
         System.out.println("Opção 1 - Incluir item");
         System.out.println("Opção 2 - Excluir item");
         System.out.println("Opção 3 - Listar itens");
+        System.out.println("Opção 4 - Sair");
     }
 }
